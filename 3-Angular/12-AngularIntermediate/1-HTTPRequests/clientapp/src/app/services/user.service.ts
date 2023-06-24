d9be0b2da6716eb2e2d234b96f64d3f13ccf4a6e
Dominic Tripodi <dtripodi93@gmail.com>
2023-06-23 22:11:43 -0400

    Added node and organization

---
+++ 3-Angular/12-AngularIntermediate/1-HTTPRequests/clientapp/src/app/services/user.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    customColor: string = "black"
+    customColorHasChanged: Subject<void> = new Subject<void>();
+    // customColorHasChanged: Subject<string> = new Subject<string>();
+
+    userList: string[] = [
+        "Tucker Anselm",
+        "Elmira Keddy",
+        "Eveline Grandisson",
+        "Berry Wildes",
+        "Quintus Hastings",
+        "Harp Antonignetti",
+        "Vite Playfair",
+        "Noelle Dowears",
+        "Delcine Lubbock",
+        "Auberta Skerrett",
+        "Constantin Cosgry",
+        "Loleta Grenfell",
+        "Nadeen Matchett",
+        "Elli Galliver",
+        "Gayla Hawtin",
+        "Liam Antwis",
+        "Merilyn Baumford",
+        "Lilas Colquyte",
+        "Roi Kinworthy",
+        "Patin Flecknoe",
+        "Etienne Vedeneev",
+        "Diane Evesque",
+        "Ashlee Amoore",
+        "Julissa Bandey",
+        "Merridie McPartling",
+        "Nanete Kitlee"        
+    ]
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    getUsers() {
+        return this.http.get("http://localhost:8080/user/users")
+    }
+
+    editUser(fullName: string, index: number) {
+        this.userList[index] = fullName;
+    }
+}
---
+++ 3-Angular/12-AngularIntermediate/5-HTTPEdit/clientapp/src/app/services/user.service.ts
@@ -0,0 +1,68 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { User } from "src/app/models/User";
+import { Subject } from "rxjs";
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    customColor: string = "black"
+    customColorHasChanged: Subject<void> = new Subject<void>();
+    emptyUser = {
+        userId: 0,
+        username: "",
+        fullName: "",
+        city: "",
+        gender: "",
+        favoriteColor: "",
+        favoriteAnimal: ""
+    };
+    // customColorHasChanged: Subject<string> = new Subject<string>();
+
+    // userList: string[] = [
+    //     "Tucker Anselm",
+    //     "Elmira Keddy",
+    //     "Eveline Grandisson",
+    //     "Berry Wildes",
+    //     "Quintus Hastings",
+    //     "Harp Antonignetti",
+    //     "Vite Playfair",
+    //     "Noelle Dowears",
+    //     "Delcine Lubbock",
+    //     "Auberta Skerrett",
+    //     "Constantin Cosgry",
+    //     "Loleta Grenfell",
+    //     "Nadeen Matchett",
+    //     "Elli Galliver",
+    //     "Gayla Hawtin",
+    //     "Liam Antwis",
+    //     "Merilyn Baumford",
+    //     "Lilas Colquyte",
+    //     "Roi Kinworthy",
+    //     "Patin Flecknoe",
+    //     "Etienne Vedeneev",
+    //     "Diane Evesque",
+    //     "Ashlee Amoore",
+    //     "Julissa Bandey",
+    //     "Merridie McPartling",
+    //     "Nanete Kitlee"        
+    // ]
+
+    userList: User[] = [];
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    getUsers() {
+        return this.http.get<User[]>("http://localhost:8080/user/users")
+    }
+
+    // editUser(fullName: string, index: number) {
+    editUser(updatedUser: User, index: number) {
+        this.userList[index] = updatedUser;
+    }
+
+    editUserInAPI(userForEdit: User) {
+        return this.http.put<User>("http://localhost:8080/user/userEdit", userForEdit)
+    }
+}
