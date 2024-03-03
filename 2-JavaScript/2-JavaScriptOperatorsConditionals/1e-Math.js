bacb1e5fe3a726a526c281adcc610b683663dab0
Dominic Tripodi <dtripodi93@gmail.com>
2024-03-03 02:20:52 -0500

    Added Solutions for first coding exercises

---
+++ 2-JavaScript/2-JavaScriptOperatorsConditionals/1e-Math.js
@@ -0,0 +1,18 @@
+let fifteen = 15;
+let twelve = 12;
+let five = 5;
+
+//The sum of 15 plus 5 
+console.log(fifteen + five);
+
+//The difference between 15 and 5 
+console.log(fifteen - five);
+
+//The product of 15 times 5 
+console.log(fifteen * five);
+
+//The quotient of 15 divided by 5 
+console.log(fifteen / five);
+
+//The remainder of 12 divided by 5
+console.log(twelve % five);
---
+++ 2-JavaScript/2-JavaScriptOperatorsConditionals/2e-ConditionalOperators.js
@@ -0,0 +1,18 @@
+let ten = 10;
+let tenString = "10";
+
+let seven = 7
+let fourteen = 14
+
+//Whether 14 is less than 7
+console.log(fourteen < seven);
+
+//Whether 14 is greater than than 7
+console.log(fourteen > seven);
+
+//Whether 10 is implicitly equal to "10"
+console.log(ten == tenString);
+
+//Whether 10 is explicitly equal to "10"
+console.log(ten === tenString);
+
