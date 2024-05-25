5a20ba8001b3074e050611bfb1d0ffcdb35e4aee
Dominic Tripodi <dtripodi93@gmail.com>
2024-05-25 12:59:18 -0400

    Updated For Angular 18

--- 3-Angular/1-AngularBasic/1-Unchanged/clientapp/src/app/app.component.spec.ts
+++ 3-Angular/1-AngularBasic/1-Unchanged/clientapp/src/app/app.component.spec.ts
@@ -1,10 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -22,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('clientapp app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, clientapp');
   });
 });
--- 3-Angular/1-AngularBasic/1-Unchanged/clientapp/src/app/app.component.ts
+++ 3-Angular/1-AngularBasic/1-Unchanged/clientapp/src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'clientapp';
